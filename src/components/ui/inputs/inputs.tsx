dfe7ddc4a31c8d1ad3a59cd7f0cc62f2c782490f
talerqa <hitri3003@gmail.com>
2023-09-22 10:06:59 +0300

    refactoring: style input

--- src/components/ui/inputs/inputs.tsx
+++ src/components/ui/inputs/inputs.tsx
@@ -5,7 +5,7 @@ import {
   forwardRef,
   useState
 } from "react";
-import {Flex, Text, TextField} from '@radix-ui/themes';
+import { Text, TextField} from '@radix-ui/themes';
 import s from './inputs.module.scss'
 import {
   EyeNoneIcon,
@@ -27,13 +27,17 @@ export type InputProps<T extends ElementType = "input"> = {
 
 export const Inputs = forwardRef<HTMLInputElement, InputProps>((props, ref): JSX.Element => {
   const {
-    type = 'password', error, disabled, label, placeholder, variant = 'default'
+    type = 'text',
+    error,
+    disabled,
+    label,
+    placeholder,
+    variant = 'default'
   } = props;
 
   // const [openText, setOpenText] = useState(showText)
-  const [value, setvalue] = useState('');
+  const [value, setValue] = useState('');
   const [typeInput, setTypeInput] = useState(type)
-
   const [focus, setFocus] = useState<boolean>(false)
 
   const showHandler = () => {
@@ -41,10 +45,10 @@ export const Inputs = forwardRef<HTMLInputElement, InputProps>((props, ref): JSX
   }
 
   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setvalue(e.currentTarget.value)
+    setValue(e.currentTarget.value)
 
   }
-  // variant?: "default" | "defaultToggle" | "defaultSearch";
+
   const result =
     variant === 'search' && !error
       ? s.searchInput
@@ -54,50 +58,43 @@ export const Inputs = forwardRef<HTMLInputElement, InputProps>((props, ref): JSX
           ? s.input + ' ' + s.error
           : s.input;
 
-
   const handler = () => setFocus(focus => !focus)
   const onBlurHandler = () => {
     setFocus(false)
   }
 
-  return (<div>
-    <Flex>
+  return (<div className={s.inputBlock}>
       <Text className={s.label}>{variant === 'search' ? '' : label}</Text>
       <TextField.Slot className={s.slot}>
-
-
         {variant === 'default' || variant === 'search'
           ? ''
           : disabled
-            ? <EyeOpenIcon className={s.eyeOpenIconDisabled}
-                           onClick={showHandler}/>
-            : typeInput === 'password' ? <EyeNoneIcon className={s.eyeOpenIcon}
-                                                      onClick={showHandler}/>
-          : <EyeOpenIcon className={s.eyeOpenIcon}
-                      onClick={showHandler}/> }
-
-      {variant === 'search'
-        ? <MagnifyingGlassIcon
-          className={focus ? s.searchFocused : s.search}/> : ''}
-
-    </TextField.Slot>
-    <TextField.Root className={s.root}>
-      <TextField.Input
-        onFocus={handler}
-        onChange={onChangeHandler}
-        onBlur={onBlurHandler}
-        ref={ref}
-        value={value}
-        className={result}
-        placeholder={placeholder}
-        size="1"
-        disabled={disabled}
-        type={typeInput}/>
-    </TextField.Root>
-  </Flex>
-{
-  error && <Text className={s.labelError}>Error!</Text>
-}
-</div>
-)
+            ? <EyeOpenIcon
+              className={s.eyeOpenIcon + ' ' + s.eyeOpenIconDisabled}
+              onClick={showHandler}/>
+            : typeInput === 'password'
+              ? <EyeNoneIcon className={s.eyeOpenIcon}
+                             onClick={showHandler}/>
+              : <EyeOpenIcon className={s.eyeOpenIcon}
+                             onClick={showHandler}/>}
+        {variant === 'search'
+          ? <MagnifyingGlassIcon
+            className={focus ? s.search + ' ' + s.searchFocused : s.search}/>
+          : ''}
+      </TextField.Slot>
+      <TextField.Root className={s.root}>
+        <TextField.Input
+          onFocus={handler}
+          onChange={onChangeHandler}
+          onBlur={onBlurHandler}
+          ref={ref}
+          value={value}
+          className={result}
+          placeholder={placeholder}
+          size="1"
+          disabled={disabled}
+          type={typeInput}/>
+      </TextField.Root>
+      {error && <Text className={s.labelError}>Error!</Text>}</div>
+  )
 })
