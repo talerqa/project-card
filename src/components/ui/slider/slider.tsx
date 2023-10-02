918f84e972ef3e8562e5d06a7fcbed97bc77221e
talerqa <hitri3003@gmail.com>
2023-10-02 12:19:08 +0300

    add: ref and values for slider

--- src/components/ui/slider/slider.tsx
+++ src/components/ui/slider/slider.tsx
@@ -12,6 +12,7 @@ export type SelectProps = {
   variant?: "default";
   label?: 'Slider'
   value: number[]
+  step: number
   array?: { title: string, value: string }[]
   placeholder?: string
   disabled: boolean
@@ -20,98 +21,53 @@ export type SelectProps = {
 export const Slider = forwardRef<ElementRef<typeof SliderGroup.Root>, SelectProps>((props, ref) => {
   const {
     value,
-    // label,
-    // placeholder,
-    // array,
-    // value,
-    // disabled
+    step,
   } = props;
 
   const [min, setMin] = useState<number>(value[0])
   const [max, setMax] = useState(value[1])
 
-  // const [open, setOpen] = useState<boolean>(false)
-
-
-  const onChangeValueMin = (e: ChangeEvent<HTMLInputElement>) => {
-    if (Number(e.currentTarget.value) < max) {
-      setMin(Number(e.currentTarget.value))
-    }
+  const onHandler = (ref: any) => {
+    setMin(ref[0])
+    setMax(ref[1])
   }
-  const onChangeValueMax = (e: ChangeEvent<HTMLInputElement>) => {
 
+  const onChangeMinHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setMin(Number(e.currentTarget.value))
+  }
+
+  const onChangeMaxHandler = (e: ChangeEvent<HTMLInputElement>) => {
     setMax(Number(e.currentTarget.value))
-
   }
 
-  const onHandler = () => {
-    console.log(value)
-  }
 
+  console.log(min, max)
 
   return (<div className={s.selectBlock}>
-      <input type="text" value={min} onChange={onChangeValueMin}
+      <input type="text" value={min} onChange={onChangeMinHandler}
              className={s.inputValue}/>
-      <SliderGroup.Root className={s.SliderRoot} defaultValue={value}
+      <SliderGroup.Root className={s.SliderRoot}
+                        defaultValue={value}
+                        value={[min, max]}
+                        max={value[1]}
+                        minStepsBetweenThumbs={step}
                         ref={ref}
                         onValueChange={onHandler}
-                        step={1}>
+                        step={step}>
         <SliderGroup.Track className={s.SliderTrack}>
           <SliderGroup.Range className={s.SliderRange}/>
         </SliderGroup.Track>
-        <SliderGroup.Thumb className={s.SliderThumb} aria-label="Volume"/>
-        <SliderGroup.Thumb className={s.SliderThumb} aria-label="Volume"
-                           ref={ref}/>
+        {value.map((i: any, index: number) => {
+          return <SliderGroup.Thumb key={index} className={s.SliderThumb}
+                                    aria-label="Volume"
+          />
+        })}
+
       </SliderGroup.Root>
-      <input type="text" value={max} onChange={onChangeValueMax}
+      <input type="text" value={max} onChange={onChangeMaxHandler}
              className={s.inputValue}/>
     </div>
 
-
-
-    // return (<div className={s.selectBlock}>
-    //     <SelectGroup.Root value={value} onOpenChange={handlerOpenedMenu}
-    //                       disabled={disabled}>
-    //       <span
-    //         className={disabled ? s.textLabelDisabled : s.textLabel}>{label}</span>
-    //       <SelectGroup.Trigger className={s.button} tabIndex={0} ref={ref}>
-    //         <div className={s.selectValueBlock}>
-    //           <div
-    //             className={disabled ? s.disabled + " " + s.valuePlaceholder : s.valuePlaceholder}>
-    //             <SelectGroup.Value
-    //               placeholder={placeholder}
-    //             />
-    //           </div>
-    //           {open ? <ChevronUpIcon
-    //               className={disabled ? s.iconDisabled + ' ' + s.icon : s.icon}/> :
-    //             <ChevronDownIcon
-    //               className={disabled ? s.iconDisabled + ' ' + s.icon : s.icon}/>}
-    //         </div>
-    //       </SelectGroup.Trigger>
-    //       <SelectGroup.Content position="popper" className={s.content}>
-    //
-    //         <SelectGroup.Viewport className={s.viewport}>
-    //           <SelectGroup.Group className={s.items}>
-    //             {array?.map((item: any) => {
-    //               return <Item children={item.title} value={item.value}/>
-    //             })}
-    //           </SelectGroup.Group>
-    //         </SelectGroup.Viewport>
-    //       </SelectGroup.Content>
-    //     </SelectGroup.Root>
-    //   </div>
   )
 })
-//
-// type Props = {
-//   children: string
-//   value?: string
-// }
-// export const Item = (props: Props) => {
-//   const {children} = props
-//   return (<SelectGroup.Item value={children} className={s.item}>
-//     <SelectGroup.ItemText
-//       className={s.itemText}>{children}</SelectGroup.ItemText>
-//   </SelectGroup.Item>)
-// }
-//
+
