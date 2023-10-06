efee2681fc665837537a9102e3fead4fabfb3751
RomanMorozenko <romanvm2020@gmail.com>
2023-10-06 22:30:58 +0700

    button refactored

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,51 +1,63 @@
 import {
   ButtonHTMLAttributes,
   ComponentPropsWithoutRef,
-  ElementType, HTMLAttributes
+  ElementType,
+  HTMLAttributes,
 } from "react";
+
 import s from "./button.module.scss";
 import iconExit from "./img/exit.svg";
 import iconExitDisabled from "./img/exitDisabled.svg";
 
 export type ButtonProps<T extends ElementType = "button"> = {
   as?: T;
-  variant?: "primary" | "secondary" | 'primaryWithIcon' | "tertiary" | "link" | 'secondaryWithIcon';
+  variant?:
+    | "primary"
+    | "secondary"
+    | "primaryWithIcon"
+    | "tertiary"
+    | "link"
+    | "secondaryWithIcon";
   className?: string;
-  children?: string
-  classNameText?: string
-  icon: boolean
-  type?: ButtonHTMLAttributes<HTMLAttributes<T>>['type']
+  children?: string;
+  classNameText?: string;
+  icon: boolean;
+  type?: ButtonHTMLAttributes<HTMLAttributes<T>>["type"];
 } & ComponentPropsWithoutRef<T>;
 
 export const Button = <T extends ElementType = "button">(
   props: ButtonProps<T> &
-    Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>,
+    Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>
 ) => {
   const {
     variant = "primary",
-    className, icon,
-    classNameText, children, disabled,
+    className,
+    icon,
+    classNameText,
+    children,
+    disabled,
     as: Component = "button",
     type,
     ...rest
   } = props;
 
-  return (<div className={s.button}>
+  return (
+    <div className={s.button}>
       <Component
         className={`${s[variant]} ${className}`}
         type={type}
-        children={<>
-          {icon && <img src={!disabled ? iconExit : iconExitDisabled} alt=""
-                        className={s.icon}/>}
-          {variant !== 'link' &&
-              <span className={classNameText}>{children}</span>
-          }
-          {variant === 'link' && <>{children}</>}
-        </>
-        }
         disabled={disabled}
         {...rest}
       />
+      {icon && (
+        <img
+          src={!disabled ? iconExit : iconExitDisabled}
+          alt=""
+          className={s.icon}
+        />
+      )}
+      {variant !== "link" && <span className={classNameText}>{children}</span>}
+      {variant === "link" && <>{children}</>}
     </div>
   );
 };
