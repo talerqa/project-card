ea3f60836974ee166aac4bff6307b0ccf3fade36
talerqa <hitri3003@gmail.com>
2023-10-06 12:08:17 +0300

    add: drop down menu with avatar

--- src/components/ui/dropdown/dropdown.stories.tsx
+++ src/components/ui/dropdown/dropdown.stories.tsx
@@ -1,8 +1,11 @@
 import type {Meta, StoryObj} from "@storybook/react";
-import {DropDown} from "./";
+import {DropDown, ItemDropDown, ProfileDropDown} from "./";
+import avatar from "./../../../assets/img/avatart-template.png"
+import person from "./../../../assets/img/person.svg"
+import logout from "./../../../assets/img/exit.svg"
 
 const meta = {
-  title: "Components/Slider",
+  title: "Components/DropDown",
   component: DropDown,
   tags: ["autodocs"],
 
@@ -12,30 +15,31 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 
-
 export const DropDownMenu: Story = {
   args: {
     label: "DropDown",
-    children: '',
+    children: (
+      <>
+
+      </>
+    ),
+    align: 'end',
+    trigger: (<button>!!!!</button>)
   }
 };
 
 export const DropDownMenuWithIcon: Story = {
-  args: {
-    label: "DropDownWithIcon",
-    children: '',
+    args: {
+      label: "DropDownWithIcon",
+      children: (<>
+          <ProfileDropDown img={avatar}/>
+          <ItemDropDown img={person} title={'My Profile'}/>
+          <ItemDropDown img={logout} title={'Sign Out'}/>
+        </>
+      ),
+      align: 'end',
+      trigger: (<button><img src={avatar} alt=""/></button>)
+    }
   }
-};
+;
 
-
-// const SliderWithUseState = (args: SliderProps) => {
-//   const [min, setMin] = useState<number>(args.value[0])
-//   const [max, setMax] = useState<number>(args.value[1])
-//
-//   const onHandler = (ref: number[]) => {
-//     setMin(ref[0])
-//     setMax(ref[1])
-//   }
-//
-//   return <Slider {...args} min={min} max={max} onValueChange={onHandler}/>
-// }
--- src/components/ui/dropdown/dropdown.tsx
+++ src/components/ui/dropdown/dropdown.tsx
@@ -1,29 +1,126 @@
-import {ComponentPropsWithoutRef, ElementRef, forwardRef,} from "react";
+import React, {
+  ComponentPropsWithoutRef,
+  ElementRef,
+  forwardRef,
+  ReactNode,
+  useState,
+} from "react";
 import s from './dropdown.module.scss'
-import * as SliderGroup from '@radix-ui/react-slider';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import {Typography} from "@/components/ui/typography";
 
 export type SliderProps = {
   label: "DropDown" | "DropDownWithIcon"
-  children: string
+  align?: 'start' | 'center' | 'end'
+  children: ReactNode
+  trigger: ReactNode
+  className?: string
   // value: number[]
   // step: number
   // minStepsBetweenThumbs: number
-} & ComponentPropsWithoutRef<typeof SliderGroup.Root>;
+} & ComponentPropsWithoutRef<typeof DropdownMenu.Root>;
 
-export const DropDown = forwardRef<ElementRef<typeof SliderGroup.Root>, SliderProps>((props, ref) => {
+export const DropDown = forwardRef<ElementRef<typeof DropdownMenu.Root>, SliderProps>((props, ref) => {
   const {
-    label, children,
+    label, children, trigger, align,
     ...res
   } = props;
 
-  return (<div className={s.sliderBlock}>
-      <Typography variant={"body1"} as={"p"}
-                  className={s.valueText}/>
+  const [open, setOpen] = useState<boolean>(false)
 
-      <Typography variant="body1" as="span"
-                  className={s.valueText}/>
-    </div>
-  )
+  const className = {
+    trigger: s.trigger,
+    content: s.content,
+    root: s.root,
+    portal: s.portal,
+    item: s.item,
+    arrow: s.arrow,
+
+  }
+
+
+  return (
+    <div className={className.root}>
+
+      <DropdownMenu.Root open={open} onOpenChange={setOpen}>
+        <DropdownMenu.Trigger asChild className={className.trigger}>
+          {trigger}
+        </DropdownMenu.Trigger>
+
+        {open && <DropdownMenu.Portal>
+            <DropdownMenu.Content
+                align={align}
+                onClick={event => event.stopPropagation()}
+                className={className.content}>
+                <DropdownMenu.Label>
+                    <DropdownMenu.Item className={className.item}>
+                      {children}
+
+                    </DropdownMenu.Item>
+                </DropdownMenu.Label>
+                <DropdownMenu.Arrow className={className.arrow}>
+                </DropdownMenu.Arrow>
+            </DropdownMenu.Content>
+        </DropdownMenu.Portal>}
+      </DropdownMenu.Root>
+    </div>)
 })
 
+
+type ProfileDropDown = {
+  img: string
+}
+const dataProfile = {
+  name: 'Ivan',
+  email: 'j&johnson@gmail.com'
+}
+
+const classNameProfile = {
+  profileDropDownBlock: s.profileDropDownBlock,
+  itemsBlock: s.itemsBlock,
+  emailText: s.emailText,
+  profileImg: s.profileImg,
+  nameText: s.nameText,
+
+}
+
+export const ProfileDropDown: React.FC<ProfileDropDown> = (props) => {
+  const {img} = props
+  return <div className={classNameProfile.profileDropDownBlock}>
+    <img src={img} alt="img-avatart" className={classNameProfile.profileImg}/>
+    <div className={classNameProfile.itemsBlock}>
+      <Typography variant={"subtitle2"} as={'p'}
+                  children={dataProfile.name}
+                  className={classNameProfile.nameText}/>
+      <Typography variant={"caption"} as={'p'} children={dataProfile.email}
+                  className={classNameProfile.emailText}/>
+    </div>
+  </div>
+}
+
+
+type ItemDropDownProps = {
+  img: string
+  title: string
+}
+
+const ItemDropDownClassName = {
+  itemDropDown: s.itemDropDownBlock,
+  itemDropDownContent: s.itemDropDownContent,
+  imageSvg: s.imageSvg,
+  title: s.title,
+  separator: s.separator,
+}
+
+export const ItemDropDown: React.FC<ItemDropDownProps> = (props) => {
+
+  const {img, title} = props
+  return <div className={ItemDropDownClassName.itemDropDown}>
+    <DropdownMenu.Separator className={ItemDropDownClassName.separator}/>
+    <div className={ItemDropDownClassName.itemDropDownContent}>
+      <img src={img} alt="svg-icon" className={ItemDropDownClassName.imageSvg}/>
+      <Typography variant={'caption'} as={'p'} children={title}
+                  className={ItemDropDownClassName.title}/>
+    </div>
+  </div>
+}
