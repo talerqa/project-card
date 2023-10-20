9163375b4996520ac726e5d7b2001a1fbef8c6e2
talerqa <hitri3003@gmail.com>
2023-10-20 16:30:21 +0300

    add: sign in and sign up pages, layout component

---
+++ src/pages/auth/signInPage/signInPage.tsx
@@ -0,0 +1,9 @@
+import {Page} from "@/components/ui/page";
+import {SignIn} from "@/components/auth/signIn";
+
+export const SignInPage = () => {
+  return (<Page>
+      <SignIn onSubmit={()=>{}}/>
+    </Page>
+  )
+}
