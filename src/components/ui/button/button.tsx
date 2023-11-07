6295f8dfc7dab5080559ca8d136a3d0d37c82d1c
RomanMorozenko <romanvm2020@gmail.com>
2023-11-07 12:38:02 +0700

    refactoring in button component

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -5,28 +5,35 @@ import {
   HTMLAttributes,
   ReactNode,
 } from "react";
+
 import s from "./button.module.scss";
 
 export type ButtonProps<T extends ElementType = "button"> = {
   as?: T;
-  variant?: | "primary" | "secondary" | "primaryWithIcon" | "tertiary" | 'link' | 'secondaryWithIcon'
-  children?: ReactNode
+  variant?:
+    | "primary"
+    | "secondary"
+    | "primaryWithIcon"
+    | "tertiary"
+    | "link"
+    | "secondaryWithIcon";
+  children?: ReactNode;
   icon?: ReactNode;
-  onClick?: any
-  className?: string
+  onClick?: any;
+  className?: string;
   type: ButtonHTMLAttributes<HTMLAttributes<T>>["type"];
 } & ComponentPropsWithoutRef<T>;
 
 //ref???
 
 export const Button = <T extends ElementType = "button">(
-  props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>,
-    keyof ButtonProps<T>>
+  props: ButtonProps<T> &
+    Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>,
 ): JSX.Element => {
   const {
     variant = "primary",
     as: Component = "button",
-    type = 'button',
+    type = "button",
     icon,
     children,
     onClick,
@@ -35,15 +42,19 @@ export const Button = <T extends ElementType = "button">(
     ...res
   } = props;
 
-  return (<div className={s.button}>
+  return (
+    <div className={s.button}>
       <Component
         className={`${s[variant]} ${className}`}
         type={type}
         disabled={disabled}
         onClick={onClick}
-        children={<>{icon} {children}</>}
         {...res}
-      />
+      >
+        <>
+          {icon} {children}
+        </>
+      </Component>
     </div>
   );
-}
+};
